63e47f421d9f03184dbfa6fffe055589d42c2562
delete1212 <yug3657@naver.com>
2024-11-06 16:54:23 +0900

    CSS 인식 안됨

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static(__dirname + '/CSS'))
+app.use(express.static(__dirname + '/HTML'))
+
 app.listen(8080, () => {
     console.log('http://localhost:8080 에서 서버 실행중')
 })
 
 app.get('/', (요청, 응답) => {
-    응답.send('반갑다')
+    응답.sendfile(__dirname + '/index.html')
+})
+
+app.get('/news', (요청, 응답) => {
+    응답.send('흐림')
+})
+
+app.get('/shop', function(요청, 응답){
+    응답.send('쇼핑')
+})
+
+app.get('/about', (요청, 응답) => {
+    응답.sendfile(__dirname + '/about.html')
 })
