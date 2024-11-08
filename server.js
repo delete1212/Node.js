2c562192383a7fc6e236ede8796bc3103e9ff7b4
delete1212 <yug3657@naver.com>
2024-11-08 12:05:40 +0900

    db연결, 데이터 입출력

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ const app = express()
 
 app.use(express.static(__dirname + '/public'))
 
-app.listen(8080, () => {
+const { MongoClient } = require('mongodb')
+
+let db
+const url = 'mongodb+srv://yug3657:Cupa3659@mechacluster.ryvdc.mongodb.net/?retryWrites=true&w=majority&appName=MechaCluster'
+new MongoClient(url).connect().then((client)=>{
+  console.log('DB연결성공')
+  db = client.db('forum')
+  app.listen(8080, () => {
     console.log('http://localhost:8080 에서 서버 실행중')
+})
+}).catch((err)=>{
+  console.log(err)
 })
 
 app.get('/', (요청, 응답) => {
@@ -12,6 +22,7 @@ app.get('/', (요청, 응답) => {
 })
 
 app.get('/news', (요청, 응답) => {
+    db.collection('post').insertOne({title : '흠'})
     응답.send('흐림')
 })
 
@@ -21,4 +32,10 @@ app.get('/shop', function(요청, 응답){
 
 app.get('/about', (요청, 응답) => {
     응답.sendfile(__dirname + '/public/HTML/about.html')
+})
+
+app.get('/list', async (요청, 응답) => {
+    let result = await db.collection('post').find().toArray()
+    console.log(result[0].title)
+    응답.send(result[0].content)
 })
