b1ba761ad78d43ffc9f326b9b220eb329aaf8f7b
delete1212 <yug3657@naver.com>
2025-01-15 15:52:32 +0900

    뽑힌 카드 표시

--- public/game/card.js
+++ public/game/card.js
@@ -37,9 +37,17 @@ export function selectCard(suit, value) {
   return selectedCard
 }
 
+export function wait(sec){
+  let start = Date.now(), now = start
+  while (now - start < sec * 1000) {
+    now = Date.now()
+  }
+}
+
 // 모듈 내보내기
 export default  {
   createDeck,
   drawCard,
   selectCard,
+  wait,
 }
